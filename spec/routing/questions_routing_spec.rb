0391f8132ca36091aa28ae44a88a2ed3d8571331
Josh Lipps <josh.lipps@lucidworks.com>
2016-08-06 12:21:45 -0700

    moved questions api behind /api

--- spec/routing/questions_routing_spec.rb
+++ spec/routing/questions_routing_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe QuestionsController, type: :routing do
   describe "routing" do
 
     it "routes to #index" do
-      expect(:get => "/questions").to route_to("questions#index")
+      expect(:get => "/api/questions").to route_to("questions#index")
     end
 
     # it "routes to #new" do
@@ -12,7 +12,7 @@ RSpec.describe QuestionsController, type: :routing do
     # end
 
     it "routes to #show" do
-      expect(:get => "/questions/1").to route_to("questions#show", :id => "1")
+      expect(:get => "/api/questions/1").to route_to("questions#show", :id => "1")
     end
 
     # it "routes to #edit" do
@@ -20,19 +20,19 @@ RSpec.describe QuestionsController, type: :routing do
     # end
 
     it "routes to #create" do
-      expect(:post => "/questions").to route_to("questions#create")
+      expect(:post => "/api/questions").to route_to("questions#create")
     end
 
     it "routes to #update via PUT" do
-      expect(:put => "/questions/1").to route_to("questions#update", :id => "1")
+      expect(:put => "/api/questions/1").to route_to("questions#update", :id => "1")
     end
 
     it "routes to #update via PATCH" do
-      expect(:patch => "/questions/1").to route_to("questions#update", :id => "1")
+      expect(:patch => "/api/questions/1").to route_to("questions#update", :id => "1")
     end
 
     it "routes to #destroy" do
-      expect(:delete => "/questions/1").to route_to("questions#destroy", :id => "1")
+      expect(:delete => "/api/questions/1").to route_to("questions#destroy", :id => "1")
     end
 
   end
