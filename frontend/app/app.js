47cc704b2ef67f3bf4fdaa5152beae067b97360c
Josh Lipps <josh.lipps@lucidworks.com>
2016-08-06 17:18:00 -0700

    Working Questions CRUD * Update questions wired up * Delete question wired up * Edit question toggle in from working using editMap * Added simple stlysheet for questions and added a couple fixes

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -3,10 +3,12 @@ import ngSanitize from 'angular-sanitize';
 import ngMaterial from 'angular-material';
 import 'angular-material/angular-material.min.css';
 import router from 'angular-ui-router';
+import _ from 'lodash';
 
 
 import componentsModule from './components/components.js';
 import appRoutes from './app-routes.js';
 
 angular.module('app', [router, ngSanitize, ngMaterial, componentsModule])
-  .config(appRoutes);
+  .config(appRoutes)
+  .value('_', _); //add lodash into angular injection lib
--- frontend/app/components/questions/questions.js
+++ frontend/app/components/questions/questions.js
@@ -1,4 +1,6 @@
 import questionsTemplate from './questions.html';
+import './questions.css';
+
 import QuestionsService from './questions.service.js';
 
 export default angular.module('app.components.questions', [])
@@ -9,13 +11,23 @@ export default angular.module('app.components.questions', [])
   .factory('QuestionsService', QuestionsService)
   .name;
 
-function questionsController(QuestionsService, $log){
+function questionsController(QuestionsService, $log, _){
   'ngInject';
 
   let vm = this;
   //'Public' functions
-  vm.saveQuestion = saveQuestion;
+  vm.updateQuestion = updateQuestion;
+  vm.removeQuestion = removeQuestion;
+  //newQuestion
   vm.addQuestion = addQuestion;
+  vm.cancelQuestion = cancelQuestion;
+  vm.saveQuestion = saveQuestion;
+  //editQuestion
+  vm.edit = edit;
+  vm.cancelEdit = cancelEdit;
+
+  //public vars
+  vm.editMap = {};
 
   init();
 
@@ -33,11 +45,13 @@ function questionsController(QuestionsService, $log){
 
   function removeQuestion(id){
     QuestionsService.remove(id).then(
-      (response) => (_.reject(vm.questions, id)),
+      function(response){
+        //cleanup list on delete
+        vm.questions = _.reject(vm.questions, {id: id});
+      },
       //TODO better error handling
       (error) => ($log.error('Error getting questions list', error))
     );
-
   }
 
   function saveQuestion(){
@@ -47,16 +61,42 @@ function questionsController(QuestionsService, $log){
         // update list with new quesiton from server
         vm.questions.push(response.data);
 
-        //onSuccessful save clear newQuestion
-        vm.newQuestion = null;
+        //onSuccessful save cancel newQuestion form
+        cancelQuestion();
       },
       //TODO better error handling
       (error) => ($log.error('Error getting saving new question', error))
     );
   }
 
+  function updateQuestion(question){
+    QuestionsService.update(question.id, question).then(
+      function(response){
+        // update list with new question from server
+        let index = _.findIndex(vm.questions, {id: question.id});
+        vm.questions[index] = response.data;
+        cancelEdit(question.id);
+      },
+      //TODO better error handling
+      (error) => ($log.error('Error getting updating question ' + question.id, error))
+    );
+  }
+
+  function edit(id){
+    vm.editMap[id] = true;
+  }
+
+  function cancelEdit(id){
+    vm.editMap[id] = false;
+  }
+
+
   function addQuestion(){
     vm.newQuestion = {};
+  }
+
+  function cancelQuestion(){
+    vm.newQuestion = null;
   }
 
 }
