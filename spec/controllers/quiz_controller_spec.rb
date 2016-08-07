830b2e0e07da2032e74ec59abba64018e1c26d28
Josh Lipps <josh.lipps@lucidworks.com>
2016-08-07 07:25:49 -0700

    Added better tests for posting numeric strings

--- spec/controllers/quiz_controller_spec.rb
+++ spec/controllers/quiz_controller_spec.rb
@@ -8,7 +8,20 @@ RSpec.describe QuizController, type: :controller do
       content: 'Is this an example question?',
       answer: 'yes'
     }
+  }
 
+  let(:answer_with_number) {
+    {
+      content: 'Whats after four',
+      answer: 5
+    }
+  }
+
+  let(:answer_with_ballons) {
+    {
+      content: 'Whats after four',
+      answer: 'five ballons'
+    }
   }
 
   let(:invalid_attributes) {
@@ -18,11 +31,51 @@ RSpec.describe QuizController, type: :controller do
     }
   }
 
+  let(:valid_session) { {} }
+
   describe "GET #index" do
     it "assigns question as @quiz" do
       question = Question.create! valid_attributes
-      get :index, params: {}
+      get :index, params: {}, session: valid_session
       expect(assigns(:quiz)).to eq([question])
+    end
+  end
+
+
+  describe "PUT #update" do
+    it "returns correct true when answer matches" do
+      question = Question.create! valid_attributes
+      put :update, params: {id: question[:id], answer: 'yes'}, session: valid_session
+      parsed_body = JSON.parse(response.body)
+      expect(parsed_body).to eq({"correct" => true})
+    end
+
+    it "correct when answer matches as number" do
+      question = Question.create! answer_with_number
+      put :update, params: {id: question[:id], answer: 5}, session: valid_session
+      parsed_body = JSON.parse(response.body)
+      expect(parsed_body).to eq({"correct" => true})
+    end
+
+    it " correct when submitted answer is a written-out number" do
+      question = Question.create! answer_with_number
+      put :update, params: {id: question[:id], answer: 'five'}, session: valid_session
+      parsed_body = JSON.parse(response.body)
+      expect(parsed_body).to eq({"correct" => true})
+    end
+
+    it "incorrect when submitted answer is a written-out number" do
+      question = Question.create! answer_with_number
+      put :update, params: {id: question[:id], answer: 'six'}, session: valid_session
+      parsed_body = JSON.parse(response.body)
+      expect(parsed_body).to eq({"correct" => false})
+    end
+
+    it "correct when submitted answer is a written-out number and string" do
+      question = Question.create! answer_with_ballons
+      put :update, params: {id: question[:id], answer: '5 ballons'}, session: valid_session
+      parsed_body = JSON.parse(response.body)
+      expect(parsed_body).to eq({"correct" => true})
     end
   end
 end
